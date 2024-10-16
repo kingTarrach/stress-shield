fca65bc425f2e0829847dae35e6c3a5d09b2a50b
EthLyoWSU <ethan.j.lyons@wsu.edu>
2024-10-15 23:58:16 -0700

    adding new base of code for older version of xcode/swift

---
+++ stress-shield-reversion/StressShield/StressShield/StressShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  StressShieldApp.swift
+//  StressShield
+//
+//  Created by Ethan on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct StressShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
